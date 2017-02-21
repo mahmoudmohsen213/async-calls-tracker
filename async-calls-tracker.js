cdb5354f540103fe2aa2cb876010dd322ed6b4f7
mahmoudmohsen213 <mahmoud.mohsen.213@outlook.com>
2017-02-22 01:59:39 +0200

    module refactored to create arbitrary number of instances

--- async-calls-tracker.js
+++ async-calls-tracker.js
@@ -2,41 +2,46 @@
 // main caller when all of them are done through the onDone function, and send
 // the arguments passed to the callbacks of the async calls as an array of
 // arguments objects
-var tracker = [];
-tracker.observedCalls = [];
-tracker.observedCallsArguments = [];
-tracker.collectedCallbackArguments = [];
-tracker.onDone = null;
+function tracker(){
+	var self = this;
+	self.observedCalls = [];
+	self.observedCallsArguments = [];
+	self.collectedCallbackArguments = [];
+	self.onDone = null;
+	
+	// set the onDoneCallback which is called when all observed async calls return
+	// the tracker pass the collected arguments in an array
+	self.setOnDoneCallback = function(onDoneCallback){
+		self.onDone = onDoneCallback;
+	}
 
-// set the onDoneCallback which is called when all observed async calls return
-// the tracker pass the collected arguments in an array
-tracker.setOnDoneCallback = function(onDoneCallback){
-	tracker.onDone = onDoneCallback;
+	// collect the arguments passed to the callbacks from the observed async calls
+	self.collectCallbackArguments = function(...args){
+		self.collectedCallbackArguments.push(args);
+		if((self.onDone)&&
+			(self.collectedCallbackArguments.length == self.observedCalls.length)){
+			self.onDone(self.collectedCallbackArguments);
+		}
+	};
+
+	// accepts the function to be observed, in addition to an arbtrary number of
+	// parameters to be passed to the observed function on its invokation
+	self.addCall = function(observedCall, ...args){
+		self.observedCalls.push(observedCall);
+		self.observedCallsArguments.push(args);
+	};
+
+	// invoke all async calls
+	self.invokeCalls = function(){
+		for(var i = 0; i < self.observedCalls.length; ++i){
+			self.observedCallsArguments[i].push(self.collectCallbackArguments);
+			self.observedCalls[i](...(self.observedCallsArguments[i]));
+		}
+	};
 }
 
-// collect the arguments passed to the callbacks from the observed async calls
-tracker.callbackArgumentsCollector = function(){
-	tracker.collectedCallbackArguments.push(arguments);
-	if((tracker.onDone)&&
-		(tracker.collectedCallbackArguments.length == tracker.observedCalls.length)){
-		tracker.onDone(tracker.collectedCallbackArguments);
-	}
-};
+function trackerFactory(){
+	return new tracker();
+}
 
-// accepts the function to be observed, in addition to an arbtrary number of
-// parameters to be passed to the observed function on its invokation
-tracker.addCall = function(observedCall){
-	var args = Array.prototype.slice.call(arguments, 1);
-	tracker.observedCalls.push(observedCall);
-	tracker.observedCallsArguments.push(args);
-};
-
-// invoke all async calls
-tracker.invokeCalls = function(){
-	for(var i = 0; i < tracker.observedCalls.length; ++i){
-		tracker.observedCallsArguments[i].push(tracker.callbackArgumentsCollector);
-		tracker.observedCalls[i](...(tracker.observedCallsArguments[i]));
-	}
-};
-
-module.exports = tracker;
+module.exports = trackerFactory;
