7b9a50298b1746dbfce08bde3721baf4ce66b6de
mahmoudmohsen213 <mahmoud.mohsen.213@outlook.com>
2017-03-05 13:57:12 +0200

    added parameters type checking and a general event handler registration method to confirm with javascript standards

--- async-calls-tracker.js
+++ async-calls-tracker.js
@@ -2,33 +2,46 @@
 // main caller when all of them are done through the onDone function, and send
 // the arguments passed to the callbacks of the async calls as an array of
 // arguments objects
+const FILE_NAME = 'async-calls-tracker';
+const END_EVENT_NAME = 'end';
+
 function tracker(){
 	var self = this;
 	self.observedCalls = [];
 	self.observedCallsArguments = [];
 	self.collectedCallbackArguments = [];
-	self.onDone = null;
+	self.eventHandlers = [];
 	
-	// set the onDoneCallback which is called when all observed async calls return
-	// the tracker pass the collected arguments in an array
-	self.setOnDoneCallback = function(onDoneCallback){
-		self.onDone = onDoneCallback;
+	// set the event handler of each event
+	self.on = function(eventName, eventHandler){
+		if(typeof eventName !== 'string')
+			throw new TypeError('eventName must be a string object', FILE_NAME);
+		
+		if(typeof eventHandler !== 'function')
+			throw new TypeError('eventHandler must be a function object', FILE_NAME);
+		
+		self.eventHandlers[eventName] = eventHandler;
+		return self;
 	}
 
 	// collect the arguments passed to the callbacks from the observed async calls
 	self.collectCallbackArguments = function(...args){
 		self.collectedCallbackArguments.push(args);
-		if((self.onDone)&&
+		if((self.eventHandlers[END_EVENT_NAME])&&
 			(self.collectedCallbackArguments.length == self.observedCalls.length)){
-			self.onDone(self.collectedCallbackArguments);
+			self.eventHandlers[END_EVENT_NAME](self.collectedCallbackArguments);
 		}
 	};
 
 	// accepts the function to be observed, in addition to an arbtrary number of
 	// parameters to be passed to the observed function on its invokation
 	self.addCall = function(observedCall, ...args){
+		if(typeof observedCall !== 'function')
+			throw new TypeError('observedCall must be a function object', FILE_NAME);
+		
 		self.observedCalls.push(observedCall);
 		self.observedCallsArguments.push(args);
+		return self;
 	};
 
 	// invoke all async calls
@@ -37,6 +50,7 @@ function tracker(){
 			self.observedCallsArguments[i].push(self.collectCallbackArguments);
 			self.observedCalls[i](...(self.observedCallsArguments[i]));
 		}
+		return self;
 	};
 }
 
